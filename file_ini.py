49af356cc036ab57d07e66e1ddb467ede5d99181
libc <bingchuan@libc.vip>
2021-04-14 22:49:25 +0800

    ini_file的学习。使用configparser模块处理“

--- file_ini.py
+++ file_ini.py
@@ -19,4 +19,28 @@ print(result)  # ['mysqld', 'mysqld_safe', 'client']
 
 # 2、获取节点下的键值
 result = config.items("mysqld_safe")
-print(result)   # [('log-error', '/var/log/mariadb/mariadb.log'), ('pid-file', '/var/run/mariadb/mariadb.pid')]
+print(result)  # [('log-error', '/var/log/mariadb/mariadb.log'), ('pid-file', '/var/run/mariadb/mariadb.pid')]
+
+for key, value in config.items("mysqld_safe"):
+    print(key, value)
+
+# 3、获取某个节点下的键对应的值
+result = config.get("mysqld", "collation-server")
+print(result)
+
+# 4、其他内容
+# 4.1、是否存在节点
+v1 = config.has_section("client")
+print(v1)
+
+# 4.2、添加一个节点
+config.add_section("group")
+config.set("group", "name", "alex")  # 添加一个节点，并为他添加一个键值对
+config.set("client", "name", "alex")  # 为现有的节点添加一个键值对，有的就改
+config.set("client", "name", "glaciers")  # 为现有的节点添加一个键值对，有的就改
+config.write(open(r"D:\functionProject\files\new.ini", mode="w", encoding="utf-8"))
+
+# 4.3、删除
+config.remove_section("client")  # 删除一个节点
+config.remove_option("mysqld", "datadir")  # 删除节点下的其中一个键值对
+config.write(open(r"D:\functionProject\files\new.ini", mode="w", encoding="utf-8"))
